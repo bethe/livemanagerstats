b7545ed327e08da6c3945e42b541455e5f280ef6
bethe <tobias.baur@gmail.com>
2016-02-05 17:06:00 +0000

    Latest data, initial stab at playing / starting predictions

--- update_data.R
+++ update_data.R
@@ -7,78 +7,29 @@
 library("sqldf")
 library("plyr")
 
-### (1) Add latest matchday data
 
-## Import Data, keep colnames
+
+### 1 Add latest matchday data
+
+
+# Import Data, keep colnames
 setwd("/home/pi/Projects/livemanager")
-load("dataprep.RData")
-raw_colnames = colnames(bl_raw)
+load("players_match.RData")
 raw = read.csv("data/playerdata.csv")
 colnames(raw) <- raw_colnames
+colnames(raw) <- gsub("\\X__", "", colnames(raw))
+colnames(raw) <- gsub("\\player_status_", "", colnames(raw))
+colnames(raw) <- gsub("\\matches_info__", "", colnames(raw))
+colnames(raw) <- gsub("\\.", "_", colnames(raw))
 
-## subset to BL only players
+
+# subset to BL only players
 bl_raw <- sqldf('SELECT * FROM raw WHERE home_squad >= 1562 AND home_squad <=1579')
 
-## Get latest round and matches played
-max_matchday <- max(bl_raw$matchday)
-round <- subset(bl_raw[c("id", "total_earnings")], bl_raw$matchday == max_matchday)
-colnames(round) <- c("id", "Round")
-totals <- sqldf('SELECT id, SUM(goal) AS G, SUM(assist) AS A, SUM(clean_sheet) AS CS, SUM(total_earnings) AS Earnings, COUNT(*) AS matches, SUM(total_earnings)/COUNT(*) AS Average
-		FROM bl_raw
-		WHERE status IN ("starter", "sub_in", "sub_out", "red_card")
-		GROUP BY id')
-webstyle <- sqldf('SELECT totals.id AS id, Earnings, Average, Round, G, A, CS 
-		  FROM round JOIN totals
-		  ON round.id = totals.id')
 
-## Update Formats and handle NAs
-webstyle$G[is.na(webstyle$G)] <- 0
-webstyle$A[is.na(webstyle$A)] <- 0
-webstyle$CS[is.na(webstyle$CS)] <- 0
-webstyle$G <- as.integer(webstyle$G)
-webstyle$A <- as.integer(webstyle$A)
+# join as plm dataset
+plm <- merge(x = bl_raw, y = players, by = "id", all.x = TRUE)
 
 
-## UPDATE EXISTING DATAFRAMES
-library("googlesheets")
-trix <- gs_title("playlivemanager players")
-names_match <- gs_read(trix, ws=12)
-
-oneliner <- merge(names_match, webstyle, by = "id")
-oneliner$Value <- oneliner$init_Value + round(floor(oneliner$Earnings / 100000 + 0.5)/10, 1)
-
-# Add Code for position for sorting later
-oneliner$poscode <- as.integer(revalue(oneliner$Pos, c("GOA" = 1, "DEF" = 2, "MID" = 3, "ATT" = 4)))
-
-
-
-# Add points earned by matchday in new dataframe "fullhouse"
-fullhouse <- oneliner
-fullhouse$Round <- NULL
-bl_rounds <- subset(bl_raw, bl_raw$id %in% fullhouse$id)
-plm <- sqldf("SELECT * FROM bl_raw t1
-                     LEFT JOIN 
-                    (SELECT id AS ID, Name, Club, Pos, init_Value FROM oneliner) t2
-                    ON t1.id = t2.ID")
-
-# get number of rounds played
-rounds = max(bl_rounds$matchday)
-
-# get columns in dataset now to rename additional columns later
-cols = length(fullhouse)
-
-# loop to get all earnings by round
-for (i in 1:rounds) {
-  round_only <-  subset(bl_rounds["total_earnings"], bl_rounds$matchday == i )
-  fullhouse <- transform(fullhouse, round = round_only)
-  # NOTE: works as long as both fullhouse & round are ordered by id
-}
-
-# loop to rename columns to "RoundX" format
-for (i in (cols+1):(cols+rounds)) {
-  colnames(fullhouse)[i] <- paste0("Round",i-cols)
-}
-
-cols2 = length(fullhouse)
-
-save(list = ls(all = TRUE), file = "dataprep.RData")
+# save
+save(plm, file = "plm.RData")
